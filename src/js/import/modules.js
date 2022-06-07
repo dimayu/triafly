e04a237c2472d059ad7fd3ef4bed733c19fc2c64
Damir Sharipzyanov <dimayu82@gmail.com>
2022-06-07 22:45:31 +0300

    start project

---
+++ src/js/import/modules.js
@@ -0,0 +1,6 @@
+import "%modules%/header/header";
+import "%modules%/footer/footer";
+import "%modules%/faq/faq";
+import "%modules%/modal/modal";
+import "%modules%/menu/menu";
+import "%modules%/tabs/tabs";
