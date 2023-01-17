9aec41aa9b8103cb9fce469c8e1cf67c7a3e3708
damir <dimayu82@gmail.com>
2023-01-17 09:54:11 +0300

    Start project

--- src/js/import/modules.js
+++
@@ -1,6 +0,0 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer";
-import "%modules%/faq/faq";
-import "%modules%/modal/modal";
-import "%modules%/menu/menu";
-import "%modules%/tabs/tabs";
